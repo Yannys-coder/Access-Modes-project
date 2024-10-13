30d56e51cf93d48a003a939948575ad71d1a8c64
Yannys-coder <ccyannys123@gmail.com>
2024-10-13 01:41:53 -0400

    first commit

---
+++ append.py
@@ -0,0 +1,4 @@
+file= open("Information.txt", "a")
+print("Opening in write mode")
+print(file.write("My favourite subject to teach is mathematics."))
+file.close()
---
+++ write.py
@@ -0,0 +1,4 @@
+file= open("Information.txt", "w")
+print("Opening in write mode")
+print(file.write("My name is Isa Shay Brown. I was born on July 16th 1982. I am 42 years old."))
+file.close()
